05cc8e63dde36495c00eff5ae61b96cc97927c34
Thomas Collardeau <thomas@collardeau.com>
2020-02-08 16:49:12 +0000

    add modal demo

---
+++ src/libs/modal/demo.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  import createModalStore from "./createStore";
+  const name = "my-modal";
+  createModalStore(name, { isOpen: false });
+</script>
+
+<script>
+  import Modal from "./modal.svelte";
+  import { getStore } from "../stores";
+  const openModal = () => getStore(name).open();
+</script>
+
+<Modal {name}>
+  <p>Hello there</p>
+</Modal>
+
+<button on:click={openModal}>Open Modal</button>
