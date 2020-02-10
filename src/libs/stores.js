9b6f0e176a502bc20fab3ba71ff0dad29bd6201a
Thomas Collardeau <thomas@collardeau.com>
2020-02-10 22:23:38 +0000

    include merge fn in main store creator

--- src/libs/stores.js
+++ src/libs/stores.js
@@ -2,16 +2,29 @@ import { writable } from "svelte/store";
 
 const stores = {};
 
-export function initStore(name = "", state = null, customize = x => x) {
+export function initStore(name = "", initialState = null, customize = x => x) {
   if (!name) {
     throw new Error("you need to pass a name to create a store");
   }
   if (stores[name]) {
     throw new Error(`the store "${name}" already exists`);
   }
-  const store = customize(writable(state));
-  stores[name] = store;
-  return store;
+  const store = writable(initialState);
+  store.merge = xs => {
+    store.update(st => {
+      if (isArray(st) && isArray(xs)) {
+        return [...st, ...xs];
+      }
+      if (isObj(st) && isObj(xs)) {
+        return { ...st, ...xs };
+      }
+      console.warn(`cannot merge ${xs} into ${st}`);
+      return st;
+    });
+  };
+  const customStore = customize(store, initialState);
+  stores[name] = customStore;
+  return customStore;
 }
 
 export function getStore(name = "") {
@@ -23,4 +36,12 @@ export function getStore(name = "") {
     throw new Error(`Store named "${name}" does not exist.`);
   }
   return store;
+}
+
+function isObj(a) {
+  return !!a && a.constructor === Object;
+}
+
+function isArray(a) {
+  return Array.isArray(a);
 }
