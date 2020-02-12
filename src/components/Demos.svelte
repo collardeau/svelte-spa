a7f5a3e94bc9b19a2ba1d7594ed5c8eade39126a
Thomas Collardeau <thomas@collardeau.com>
2020-02-12 15:16:43 +0000

    create a Demos comp

---
+++ src/components/Demos.svelte
@@ -0,0 +1,71 @@
+<script>
+  import ModalDemo from "../libs/modal/Demo.svelte";
+  import RouterDemo from "../libs/hash-router/Demo.svelte";
+  import SwiperDemo from "../libs/swiper/Demo.svelte";
+  import FormDemo from "../libs/form/Demo.svelte";
+  import RoughDemo from "../libs/roughjs/Demo.svelte";
+  import TabsDemo from "../libs/tabs/Demo.svelte";
+  import SweetAlertDemo from "../libs/sweetalert2/Demo.svelte";
+  import FirebaseDemo from "../libs/firebase/Demo.svelte";
+
+  import { getStore } from "../libs/stores";
+  const store = getStore("demo-tabs");
+</script>
+
+<style>
+  .demos {
+    margin-top: 2.5rem;
+  }
+  h3 {
+    margin-bottom: 1.5rem;
+    margin-top: 2rem;
+  }
+  section {
+    padding-bottom: 3rem;
+    margin-bottom: 2rem;
+    border-top: 1px solid #ccc;
+  }
+</style>
+
+<div class="demos">
+  {#if $store.selected === 'tab-1'}
+    <section>
+      <h3>Firebase</h3>
+      <FirebaseDemo />
+    </section>
+    <section>
+      <h3>RoughJS</h3>
+      <RoughDemo />
+    </section>
+    <section>
+      <h3>Sweet Alert 2</h3>
+      <SweetAlertDemo />
+    </section>
+
+    <section>
+      <h3>Swiper</h3>
+      <SwiperDemo />
+    </section>
+  {/if}
+  {#if $store.selected === 'tab-2'}
+    <section>
+      <h3>Router</h3>
+      <RouterDemo />
+    </section>
+
+    <section>
+      <h3>Tabs</h3>
+      <TabsDemo />
+    </section>
+
+    <section>
+      <h3>Form</h3>
+      <FormDemo />
+    </section>
+
+    <section>
+      <h3>Modal</h3>
+      <ModalDemo />
+    </section>
+  {/if}
+</div>
--- src/components/KitchenSink.svelte
+++ src/components/KitchenSink.svelte
@@ -1,6 +1,5 @@
 <script context="module">
   import createTabsStore from "../libs/tabs/createStore";
-
   const store = createTabsStore("demo-tabs", { selected: "tab-1" });
 
   const tabs = [
@@ -16,41 +15,21 @@
 </script>
 
 <script>
-  import ModalDemo from "../libs/modal/Demo.svelte";
-  import RouterDemo from "../libs/hash-router/Demo.svelte";
-  import SwiperDemo from "../libs/swiper/Demo.svelte";
-  import FormDemo from "../libs/form/Demo.svelte";
-  import RoughDemo from "../libs/roughjs/Demo.svelte";
-  import TabsDemo from "../libs/tabs/Demo.svelte";
-  import SweetAlertDemo from "../libs/sweetalert2/Demo.svelte";
-  import FirebaseDemo from "../libs/firebase/Demo.svelte";
-
   import Tabs from "../libs/tabs/Tabs.svelte";
+  import Demos from "./Demos.svelte";
 </script>
 
 <style>
-  h3 {
-    margin-bottom: 1.5rem;
-    margin-top: 2rem;
-  }
-  div {
+  .container {
     text-align: center;
-    max-width: 400px;
-    margin: 0 auto;
   }
-  section {
-    padding-bottom: 3rem;
-    margin-bottom: 2rem;
-    border-top: 1px solid #ccc;
-  }
-  .demos {
-    margin-top: 2.5rem;
+  .tabs {
+    min-width: 20rem;
   }
   :global(.tabs ul) {
     display: flex;
     justify-content: space-evenly;
   }
-
   :global(.tabs button) {
     padding: 1rem;
     width: 8rem;
@@ -58,51 +37,9 @@
   }
 </style>
 
-<div>
+<div class="container">
   <div class="tabs">
     <Tabs name="demo-tabs" {tabs} />
   </div>
-
-  <div class="demos">
-    {#if $store.selected === 'tab-1'}
-      <section>
-        <h3>Firebase</h3>
-        <FirebaseDemo />
-      </section>
-      <section>
-        <h3>RoughJS</h3>
-        <RoughDemo />
-      </section>
-      <section>
-        <h3>Sweet Alert 2</h3>
-        <SweetAlertDemo />
-      </section>
-
-      <section>
-        <h3>Swiper</h3>
-        <SwiperDemo />
-      </section>
-    {/if}
-    {#if $store.selected === 'tab-2'}
-      <section>
-        <h3>Router</h3>
-        <RouterDemo />
-      </section>
-
-      <section>
-        <h3>Tabs</h3>
-        <TabsDemo />
-      </section>
-
-      <section>
-        <h3>Form</h3>
-        <FormDemo />
-      </section>
-
-      <section>
-        <h3>Modal</h3>
-        <ModalDemo />
-      </section>
-    {/if}
-  </div>
+  <Demos />
 </div>
