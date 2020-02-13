ae36c1e0305c542862e994cb5a6e2446159e8bf7
Thomas Collardeau <thomas@collardeau.com>
2020-02-13 22:05:57 +0000

    add forecasts component

---
+++ src/components/Forecasts.svelte
@@ -0,0 +1,16 @@
+<style>
+  section {
+    height: 33vh;
+    background-color: var(--white);
+    color: var(--black);
+    margin-top: var(--gap-8);
+  }
+</style>
+
+<section>
+  <h5>The Best Forecasts in Europe</h5>
+  <ul>
+    <li>Sevilla</li>
+    <li>Lisbon</li>
+  </ul>
+</section>
