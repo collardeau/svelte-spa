8d6aefbc5d5293f91e366356fc6db64a0acbeb74
Thomas Collardeau <thomas@collardeau.com>
2020-02-13 22:07:25 +0000

    add sunny svg

---
+++ src/svgs/Sunny.svelte
@@ -0,0 +1,52 @@
+<svg
+  width="180px"
+  height="180px"
+  viewBox="0 0 180 180"
+  version="1.1"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink">
+  <!-- Generator: Sketch 44.1 (41455) - http://www.bohemiancoding.com/sketch -->
+  <title>Sunny</title>
+  <desc>Created with Sketch.</desc>
+  <defs />
+  <g
+    id="SketchAppSources.com"
+    stroke="none"
+    stroke-width="1"
+    fill="none"
+    fill-rule="evenodd">
+    <g id="Weather-Icons" transform="translate(-363.000000, -2905.000000)">
+      <g id="icons" transform="translate(58.000000, 288.000000)">
+        <g id="Sunny" transform="translate(305.000000, 2617.000000)">
+          <g id="icon">
+            <g id="sun-/-big">
+              <circle
+                id="Oval-1-Copy-2"
+                stroke-opacity="0.2"
+                stroke="#FFF308"
+                stroke-width="3"
+                cx="90"
+                cy="90"
+                r="88.5" />
+              <circle
+                id="Oval-1-Copy"
+                stroke-opacity="0.6"
+                stroke="#FFF308"
+                stroke-width="4"
+                cx="89.5"
+                cy="89.5"
+                r="77.5" />
+              <circle
+                id="Oval-1"
+                fill="#FFEC65"
+                fill-rule="evenodd"
+                cx="89.5"
+                cy="89.5"
+                r="67.5" />
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
