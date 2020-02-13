84ad41fb3863ab95ab780e3120f4969421b6584c
Thomas Collardeau <thomas@collardeau.com>
2020-02-13 19:57:05 +0000

    flesh out header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -25,16 +25,35 @@
       font-size: var(--text-6xl);
     }
   }
+  li {
+    width: var(--size-3);
+    height: var(--size-1);
+    margin: 0 auto;
+    text-align: center;
+  }
+  a {
+    display: block;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 </style>
 
 <header>
   <h1>{title}</h1>
   <nav>
-    {#if route}
-      <a href="#/">Home</a>
-    {/if}
-    {#if !route}
-      <a href="#/about">About</a>
-    {/if}
+    <ul>
+      {#if route}
+        <li>
+          <a href="#/">Home</a>
+        </li>
+      {/if}
+      {#if !route}
+        <li>
+          <a href="#/about">About</a>
+        </li>
+      {/if}
+    </ul>
   </nav>
 </header>
