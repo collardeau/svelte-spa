527cdeb14cc59e7a5bb27af3241c5e01b3f86be1
Thomas Collardeau <thomas@collardeau.com>
2020-02-14 15:47:12 +0000

    add weather template

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -3,7 +3,7 @@
     text-align: center;
   }
   .hero {
-    font-size: var(--text-3xl);
+    font-size: var(--text-2xl);
   }
   p {
     margin-top: var(--gap-7);
@@ -14,6 +14,16 @@
   <p class="hero">
     Check out the
     <a href="#/kitchen-sink">Kitchen Sink</a>
+    <br />
+    to see all
+    <strong>integrations</strong>
+    included.
+  </p>
+  <p>
+    visit the
+    <a href="https://blissful-brahmagupta-48d16e.netlify.com/">
+      weather app template
+    </a>
   </p>
   <p>
     or visit the
