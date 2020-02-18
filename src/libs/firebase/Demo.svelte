c5df2e124816f684bade1f1e3d6bfad38854067b
Thomas Collardeau <thomas@collardeau.com>
2020-02-18 22:11:04 +0000

    tweak demos

--- src/libs/firebase/Demo.svelte
+++ src/libs/firebase/Demo.svelte
@@ -3,8 +3,8 @@
   import createStore from "./createStore";
   // can only *read* a firestore collection (for now) with `.get()`
 
-  const dev = true;
-  if (dev) {
+  const offline = true;
+  if (offline) {
     // you can mock firestore responses (and work offline):
     const mockCollection = [{ message: "mock-item-1", _id: "mock-id-1" }];
     createStore("my-data", { mock: mockCollection, latency: 1000 }).get();
@@ -18,7 +18,6 @@
   import { getStore } from "../stores";
 
   const store = getStore("my-data");
-  // $: console.log($store);
   $: loading = $store.loading;
   $: items = $store.data;
 </script>
