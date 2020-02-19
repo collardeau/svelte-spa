35413c70031ad7b1ed1298957ec152de89b437a3
Thomas Collardeau <thomas@collardeau.com>
2020-02-19 17:30:10 +0000

    add blank avatar

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -5,6 +5,11 @@
 
 <div class="page">
   <section>
+    <h3>intro</h3>
+    <img src="img/blank-avatar.jpg" alt="avatar" />
+    <p>Here is what I do!</p>
+  </section>
+  <section>
     <h3>works</h3>
     <ul>
       {#each works as { href, title }}
@@ -18,13 +23,6 @@
   <section>
     <h3>social media</h3>
     <SocialMedia />
-  </section>
-  <section>
-    <h3>toolbox</h3>
-    <p>
-      demo the
-      <a href="#/kitchen-sink">kitchen sink</a>
-    </p>
   </section>
 
 </div>
@@ -43,6 +41,11 @@
   }
   h3 {
     margin-bottom: var(--gap-3);
+  }
+  img {
+    width: var(--size-4);
+    border-radius: 50%;
+    margin-bottom: var(--gap-2);
   }
   li {
     margin-bottom: var(--gap-4);
