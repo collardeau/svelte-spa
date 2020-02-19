1a78b3dcf500d6b75aa35472f0a42691557c217d
Thomas Collardeau <thomas@collardeau.com>
2020-02-19 17:32:33 +0000

    update about page

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,6 +1,17 @@
 <div class="page">
   <h3>About</h3>
-  <p>This site is a demo of the svelte-spa template.</p>
+  <p>
+    This site is a demo of the svelte-spa template:
+    <br />
+    <a href="https://github.com/collardeau/svelte-spa/tree/cv-template">
+      #cv-template
+    </a>
+    branch
+  </p>
+  <p>
+    check out the
+    <a href="#/kitchen-sink">kitchen sink</a>
+  </p>
 </div>
 
 <style>
@@ -11,6 +22,6 @@
   }
   p {
     line-height: 1.5;
-    margin-top: var(--gap-4);
+    margin-top: var(--gap-6);
   }
 </style>
