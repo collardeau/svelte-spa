282cbb804364a03346a327229707b942e5ee06f1
Thomas Collardeau <thomas@collardeau.com>
2020-02-20 15:52:40 +0000

    create a project page

---
+++ src/components/ProjectPage.svelte
@@ -0,0 +1,27 @@
+<script>
+  //
+</script>
+
+<div class="project-page">
+  <slot />
+</div>
+
+<style>
+  .project-page {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    text-align: center;
+    max-width: var(--size-8);
+    line-height: 1.4;
+  }
+  :global(.project-page h3) {
+    /* font-size: var(--text-2xl); */
+    font-weight: 300;
+  }
+  :global(.project-page h5) {
+    margin-bottom: var(--gap-4);
+    font-size: var(--text-base);
+  }
+</style>
