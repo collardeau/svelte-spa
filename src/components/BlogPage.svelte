aa539927b4fa136f44a82be5e01b71c7593231f3
Thomas Collardeau <thomas@collardeau.com>
2020-02-20 14:56:41 +0000

    add blog page component

---
+++ src/components/BlogPage.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Section from "./Section.svelte";
+  import { blogs } from "../data";
+</script>
+
+<div class="blog-page">
+  <Section title="blog posts" items={blogs} let:item>
+    {item.title}
+    <small>
+      <a href={item.href}>{item.platform}</a>
+    </small>
+  </Section>
+</div>
+
+<style>
+  .blog-page {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-size: var(--text-lg);
+  }
+  :global(.blog-page h3) {
+    font-size: var(--text-3xl);
+  }
+</style>
