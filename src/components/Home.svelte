cbb1a8faf506e33431220d3b4d4b161faa559f0e
Thomas Collardeau <thomas@collardeau.com>
2020-02-20 14:30:32 +0000

    add a section component

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,42 +1,19 @@
 <script>
   import { works, projects, blogs } from "../data";
   import Intro from "./Intro.svelte";
+  import Section from "./Section.svelte";
 </script>
 
 <div class="page">
   <section>
     <Intro />
   </section>
-  <section>
-    <h3>works</h3>
-    <ul>
-      {#each works as { href, title }}
-        <li>
-          <a {href}>{title}</a>
-        </li>
-      {/each}
-    </ul>
-  </section>
-  <section>
-    <h3>projects</h3>
-    <ul>
-      {#each projects as { href, title }}
-        <li>
-          <a {href}>{title}</a>
-        </li>
-      {/each}
-    </ul>
-  </section>
-  <!-- <section>
-    <h3>blogs</h3>
-    <ul>
-      {#each blogs as { href, title }}
-        <li>
-          <a {href}>{title}</a>
-        </li>
-      {/each}
-    </ul>
-  </section> -->
+  <Section title="works" items={works} let:item>
+    <a href={item.href}>{item.title}</a>
+  </Section>
+  <Section title="projects" items={projects} let:item>
+    <a href={item.href}>{item.title}</a>
+  </Section>
 </div>
 
 <style>
@@ -47,21 +24,5 @@
     flex-direction: column;
     justify-content: space-evenly;
     line-height: 1.4;
-  }
-  section {
-    margin: var(--gap-4) 0;
-  }
-  h3 {
-    margin-bottom: var(--gap-3);
-  }
-
-  li {
-    margin-bottom: var(--gap-2);
-  }
-  a {
-    color: var(--black);
-    padding-bottom: 0.125rem;
-    text-decoration: none;
-    border-bottom: 1px dotted;
   }
 </style>
---
+++ src/components/Section.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let title = "";
+  export let items = [];
+</script>
+
+<section class="section">
+  <h3>{title}</h3>
+  <ul>
+    {#each items as item}
+      <li>
+        <slot {item} />
+      </li>
+    {/each}
+  </ul>
+</section>
+
+<style>
+  section {
+    margin: var(--gap-4) 0;
+  }
+  h3 {
+    margin-bottom: var(--gap-3);
+  }
+  li {
+    margin-bottom: var(--gap-2);
+  }
+  :global(.section a) {
+    color: var(--black);
+    padding-bottom: 0.125rem;
+    text-decoration: none;
+    border-bottom: 1px dotted;
+  }
+</style>
