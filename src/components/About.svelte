a3b1592f73a2b74cd5d03bf760b4c932ec15f117
Thomas Collardeau <thomas@collardeau.com>
2020-02-20 18:45:00 +0000

    improve about page

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,27 +1,57 @@
-<div class="page">
-  <h3>About</h3>
-  <p>
-    This site is a demo of the svelte-spa template:
-    <br />
-    <a href="https://github.com/collardeau/svelte-spa/tree/cv-template">
-      #cv-template
-    </a>
-    branch
-  </p>
-  <p>
+<div class="about-page">
+  <section>
+    <h5>Me</h5>
+    <p>
+      I'm originally from France and have lived in New York, London, Amsterdam
+      and Barcelona.
+    </p>
+    <p>I love to build cool apps, travel and stay active.</p>
+  </section>
+  <section>
+    <h5>Contact</h5>
+    <p>
+      Please email me at:
+      <br />
+      <strong>thomas [at] collardeau [dot] com</strong>
+      <br />
+      I'd love to hear from you!
+    </p>
+  </section>
+  <section>
+    <h5>Toolbox</h5>
+    <p>
+      In my 12 years of experience, I've had the opportunity to use jQuery,
+      AngularJS, Ionic, React (a lot), Gatsby, Svelte, Sapper, and Firebase.
+    </p>
+    <p>
+      Currently,
+      <a href="https://svelte.dev">Svelte</a>
+      is my favorite front-end tool.
+    </p>
+  </section>
+  <!-- <p>
     check out the
     <a href="#/kitchen-sink">kitchen sink</a>
-  </p>
+  </p> -->
 </div>
 
 <style>
-  .page {
-    flex: 1;
-    margin-top: var(--size-1);
-    text-align: center;
-  }
   p {
     line-height: 1.5;
     margin-top: var(--gap-6);
+    max-width: var(--size-8);
+  }
+  strong {
+    font-weight: 600;
+  }
+  .about-page {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    font-size: var(--text-lg);
+  }
+  :global(.about-page h3) {
+    font-size: var(--text-3xl);
   }
 </style>
