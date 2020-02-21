09620873df187c83b47c7fc6536c4dd22dec7b0c
Thomas Collardeau <thomas@collardeau.com>
2020-02-21 21:28:23 +0000

    update home page

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,7 +1,40 @@
 <script>
-  // start coding your home page :)
+  //
 </script>
 
 <div class="page">
-  <h3>Home Page</h3>
+  <section>
+    <h3>Integrations</h3>
+    <p>
+      try out the
+      <a href="#/kitchen-sink">kitchen sink</a>
+    </p>
+  </section>
+  <section>
+    <h3>templates</h3>
+    <ul>
+      <li>
+        <a href="https://optimistic-hodgkin-832d69.netlify.com">
+          #portfolio-template
+        </a>
+      </li>
+      <li>
+        <a href="https://blissful-brahmagupta-48d16e.netlify.com/">
+          #weather-template
+        </a>
+      </li>
+    </ul>
+  </section>
+  <section>
+    <h3>More Links</h3>
+    <p>
+      check out the
+      <a href="https://github.com/collardeau/svelte-spa">repo</a>
+    </p>
+    <p>
+      visit the
+      <a href="https://svelte.dev/tutorial">svelte tutorial</a>
+    </p>
+  </section>
+
 </div>
--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -5,12 +5,6 @@
 <div class="page new-page">
   <section>
     <h3>A new page</h3>
-    <p>
-      <small>
-        visit the
-        <a href="#/kitchen-sink">kitchen sink</a>
-      </small>
-    </p>
   </section>
 </div>
 
