360345a35aa7764a847c9223d618eb0dc3006d0e
Thomas Collardeau <thomas@collardeau.com>
2020-02-23 12:51:19 +0000

    update libs

--- src/libs/hash-router/Demo.svelte
+++ src/libs/hash-router/Demo.svelte
@@ -1,14 +1,28 @@
-<script context="module">
-  // this app already uses and sets up the router
-  // import createStore from "./createStore";
-  // createStore("app-router");
-</script>
-
 <script>
-  import { getStore } from "../stores";
+  import { getContext } from "svelte";
+  import Store from "../../demo/Store.svelte";
 
-  const store = getStore("app-router");
-  $: route = $store.route || "home";
+  // note that this app uses the hash router already
+  // so we just get it from context
+  const store = getContext("app-router");
 </script>
 
-<div>Current hash route: {route}</div>
+<Store {store} />
+
+<div>
+  try some internal links:
+  <br />
+  <a href="#/kitchen-sink">#kitchen-sink</a>
+  <br />
+  <a href="#/kitchen-sink/param1">#kitchen-sink/param1</a>
+  <br />
+  {#if !$store.on}
+    <small class="warn">Note that the routing of the app is broken!</small>
+  {/if}
+</div>
+
+<style>
+  .warn {
+    color: red;
+  }
+</style>
--- src/libs/hash-router/createStore.js
+++ src/libs/hash-router/createStore.js
@@ -1,22 +1,33 @@
-import { initStore } from "../stores";
+import { writable } from "svelte/store";
+
+const initialState = {
+  on: false,
+  route: "",
+  params: []
+};
 
 const createState = () => {
   const hash = window.location.hash;
   const [route, ...params] = hash.replace(/^#\/?|\/$/g, "").split("/");
   return {
-    hash,
     route,
     params
   };
 };
 
-const customize = ({ subscribe, set }) => {
-  window.onhashchange = () => {
-    set(createState());
-  };
+export default () => {
+  const { subscribe, set, update } = writable(initialState);
   return {
-    subscribe
+    subscribe,
+    start: () => {
+      set({ ...createState(), on: true });
+      window.onhashchange = () => {
+        update(st => ({ ...st, ...createState() }));
+      };
+    },
+    stop: () => {
+      window.onhashchange = () => {};
+      update(st => ({ ...st, on: false }));
+    }
   };
 };
-
-export default name => initStore(name, createState(), customize);
