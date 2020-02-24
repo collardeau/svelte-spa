8e09d50b7054fa8aca0bf2e622d54dd032d1921c
Thomas Collardeau <thomas@collardeau.com>
2020-02-24 16:46:40 +0000

    update videos page

--- src/components/pages/Videos.svelte
+++ src/components/pages/Videos.svelte
@@ -1,15 +1,49 @@
 <script>
-  import { onMount } from "svelte";
-  import Player from "../Player.svelte";
+  import { writable } from "svelte/store";
+  import createCycleStore from "../../libs/cycle/createStore";
+  import { videos } from "../../data";
 
-  // let vidIds = ["BOYdZ0GyQFQ", "Bjnu_bnJ5Po"];
+  const store = createCycleStore(videos);
+
+  let player;
+  window.onYouTubeIframeAPIReady = () => {
+    player = new YT.Player("player", {
+      height: "390",
+      width: "640",
+      videoId: $store.item.id,
+      events: {
+        // onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange
+      }
+    });
+  };
+
+  function onPlayerStateChange(event) {
+    if (event.data === 0) {
+      playNext();
+    }
+  }
+
+  function playNext() {
+    store.next();
+    player.loadVideoById($store.item.id);
+    player.playVideo();
+  }
 </script>
+
+<svelte:head>
+  <script src="https://www.youtube.com/iframe_api">
+
+  </script>
+</svelte:head>
 
 <div class="page videos-page">
   <section>
     <h3>YouTube Videos</h3>
   </section>
-  <Player />
+  <div id="player" />
+  <div>{$store.item.title}</div>
+  <button on:click={playNext}>Next</button>
 </div>
 
 <style>
---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const videos = [
+  {
+    id: "BOYdZ0GyQFQ",
+    title: "Superblood Wolfmoon"
+  },
+  {
+    id: "ymf7DZUeVow",
+    title: "Dance of the Clairvoyants"
+  }
+];
