2a7036cd221c94c7c4c6268e18caf36581c690d0
Thomas Collardeau <thomas@collardeau.com>
2020-02-24 04:48:59 +0000

    rename Stores Demo

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import About from "./components/pages/About.svelte";
 import Home from "./components/pages/Home.svelte";
 import Page from "./components/pages/Page.svelte";
-import Stores from "./demo/Stores.svelte";
+import StoresDemo from "./demo/StoresDemo.svelte";
 
 export function match({ route }) {
   switch (route) {
-    case "stores-demo":
-      return Stores;
     case "about":
       return About;
     case "page-1":
       return Page;
+    case "stores-demo":
+      return StoresDemo;
     default:
       return Home;
   }
