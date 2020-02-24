f646d3cbaff5140fdb1cc4af027696de1d63f39b
Thomas Collardeau <thomas@collardeau.com>
2020-02-24 20:02:08 +0000

    clean up cycle demo

--- src/libs/cycle/Demo.svelte
+++ src/libs/cycle/Demo.svelte
@@ -1,7 +1,8 @@
 <script context="module">
   import createStore from "./createStore";
   import Store from "../../demo/Store.svelte";
-  const store = createStore([{ a: "thing" }, { a: "marvel" }]);
+  const store = createStore(["item 1", "item 2", "item 3"]);
 </script>
 
+<div>createStore(["item 1", "item 2", "item 3"]);</div>
 <Store {store} />
--- src/libs/cycle/createStore.js
+++ src/libs/cycle/createStore.js
@@ -2,30 +2,27 @@ import { writable } from "svelte/store";
 
 export default items => {
   const { subscribe, update } = writable({
-    items,
-    active: 0,
+    index: 0,
     item: items[0]
   });
 
   const next = () => {
     update(st => {
-      const active = cycle(st);
-      return { ...st, active, item: st.items[active] };
+      const index = cycle(st);
+      return { ...st, index, item: items[index] };
     });
   };
   return {
     subscribe,
     next
   };
-};
 
-function cycle(st) {
-  const { active, items } = st;
-  if (!items.length) {
-    return 0; // -1?
+  function cycle(st) {
+    const { index } = st;
+    if (!items.length) return 0; // -1?
+    if (index === items.length - 1) {
+      return 0;
+    }
+    return index + 1;
   }
-  if (active === items.length - 1) {
-    return 0;
-  }
-  return active + 1;
-}
+};
