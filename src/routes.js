ff0ee247feda96a8bedfcd6ce44c13f8e1bf4660
Thomas Collardeau <thomas@collardeau.com>
2020-02-25 14:13:37 +0000

    mv demo files

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import About from "./components/pages/About.svelte";
 import Home from "./components/pages/Home.svelte";
 import Videos from "./components/pages/Videos.svelte";
 import Page from "./components/pages/Page.svelte";
-import Stores from "./components/pages/Stores.svelte";
-import Comps from "./components/pages/Components.svelte";
+import Stores from "./components/demo/Stores.svelte";
+import Comps from "./components/demo/Components.svelte";
 
 export function match({ route }) {
   switch (route) {
