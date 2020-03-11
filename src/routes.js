e5965dd36a4d9e0ce73e00a371687ef6cf98fdbf
Thomas Collardeau <thomas@collardeau.com>
2020-03-11 19:59:01 +0200

    remove unused routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
 import About from "./components/pages/About.svelte";
 import Home from "./components/pages/Home.svelte";
 import Page from "./components/pages/Page.svelte";
-import Stores from "./components/pages/demos/Stores.svelte";
-import Comps from "./components/pages/demos/Comps.svelte";
 
 export function match({ route }) {
   switch (route) {
@@ -10,11 +8,6 @@ export function match({ route }) {
       return About;
     case "page-1":
       return Page;
-    // demo pages:
-    case "stores":
-      return Stores;
-    case "components":
-      return Comps;
     default:
       return Home;
   }
