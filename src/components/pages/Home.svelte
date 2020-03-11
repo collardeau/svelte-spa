8df38d4e9875ce0f79f440a97dd8d812778b8ca2
Thomas Collardeau <thomas@collardeau.com>
2020-03-11 20:00:37 +0200

    update home component

--- src/components/pages/Home.svelte
+++ src/components/pages/Home.svelte
@@ -1,32 +1,33 @@
-<script>
-  //
+<script context="module">
+  import Tiles from "../layout/Tiles.svelte";
+  import Intro from "./demos/Intro.svelte";
+  import Stores from "./demos/Stores.svelte";
+  import Comps from "./demos/Comps.svelte";
+  import Bool from "../../stores/bool/Demo.svelte";
+  import ModalDemo from "../lib/ModalDemo.svelte";
+
+  const data = {
+    comp: Intro,
+    children: [
+      {
+        comp: Stores,
+        data: "Custom stores",
+        children: [
+          {
+            comp: Bool
+          }
+        ]
+      },
+      {
+        comp: Comps,
+        children: [
+          {
+            comp: ModalDemo
+          }
+        ]
+      }
+    ]
+  };
 </script>
 
-<div class="home-page">
-  <div />
-  <section>
-    <h3>Custom Stores</h3>
-    <p>
-      demo the
-      <a href="#/stores">custom store creators</a>
-    </p>
-  </section>
-  <section>
-    <h3>Components</h3>
-    <p>
-      demo
-      <a href="#/components">components</a>
-    </p>
-  </section>
-  <div />
-</div>
-
-<style>
-  .home-page {
-    display: grid;
-    align-items: center;
-    height: 100%;
-    max-width: var(--size-8);
-    margin: 0 auto;
-  }
-</style>
+<Tiles {...data} />
