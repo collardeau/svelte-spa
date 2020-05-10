8bc84631c7db38aab1ab28f98196aa7eca944926
Thomas Collardeau <thomas@collardeau.com>
2020-05-10 17:22:00 +0100

    add some starter data

---
+++ src/pages/books/data.js
@@ -0,0 +1,62 @@
+export const books = [
+  {
+    title: "The Road to Serfdom",
+    author: "Friedrich Hayek",
+    date: 1944,
+    wiki: "https://en.wikipedia.org/wiki/The_Road_to_Serfdom",
+    slug: "the-road-to-serfdom",
+  },
+  {
+    title: "Long Walk to Freedom",
+    author: "Nelson Mandela",
+    date: 1995,
+    wiki: "https://en.wikipedia.org/wiki/Long_Walk_to_Freedom",
+    slug: "long-walk-to-freedom",
+  },
+  {
+    title: "On Freedom",
+    author: "John Stuart Mill",
+    date: 1859,
+    wiki: "https://en.wikipedia.org/wiki/On_Liberty",
+    slug: "on-freedom",
+  },
+  {
+    title: "The Gulag Archipelago",
+    author: "Aleksandr Solzhenitsyn",
+    date: 1973,
+    wiki: "https://en.wikipedia.org/wiki/The_Gulag_Archipelago",
+    slug: "the-gulag-archipelago",
+  },
+  {
+    title: "The Prince",
+    author: "Niccolò Machiavelli",
+    date: 1532,
+    wiki: "https://en.wikipedia.org/wiki/The_Prince",
+    slug: "the-prince",
+  },
+];
+
+export const bookDetails = {
+  ["the-road-to-serfdom"]: {
+    title: "The Road to Serfdom",
+    author: "Friedrich Hayek",
+    date: 1944,
+    quotes: [
+      {
+        txt:
+          "many who think themselves infinitely superior to the aberrations of Nazism and sincerely hate all its manifestations, work at the same time for ideals whose realisation would lead straight to the abhorred tyranny",
+        chapter: 1,
+      },
+      {
+        txt:
+          "It seems almost as if we did not want to understand the development which has produced totalitarianism because such an understanding might destroy some of the dearest illusions to which we are determined to cling.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "it would be a mistake to believe that the specific German rather than the socialist element produced totalitarianism.",
+        chapter: 1,
+      },
+    ],
+  },
+};
