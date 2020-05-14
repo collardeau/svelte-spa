823d0f81f196c52d770ad7ec7a818cf7977254da
Thomas Collardeau <thomas@collardeau.com>
2020-05-14 01:14:36 +0100

    add data

--- src/pages/books/data.js
+++ src/pages/books/data.js
@@ -14,6 +14,13 @@ export const books = [
     slug: "long-walk-to-freedom",
   },
   {
+    title: "Economics in One Lesson",
+    author: "Henry Hazlitt",
+    date: 1946,
+    wiki: "https://en.wikipedia.org/wiki/Economics_in_One_Lesson",
+    slug: "economics-in-one-lesson",
+  },
+  {
     title: "On Freedom",
     author: "John Stuart Mill",
     date: 1859,
@@ -55,6 +62,423 @@ export const bookDetails = {
       {
         txt:
           "it would be a mistake to believe that the specific German rather than the socialist element produced totalitarianism.",
+        chapter: 1,
+      },
+    ],
+  },
+  ["economics-in-one-lesson"]: {
+    title: "Economics in One Lesson",
+    author: "Henry Hazlitt",
+    date: 1946,
+    chapters: [
+      "The Lesson",
+      "The Broken Window",
+      "The Blessings of Destruction",
+      "Public Works Mean Taxes",
+      "Taxes Discourage Production",
+      "Credit Diverts Production",
+      "The Curse of Machine",
+      "Spread-the-Work Schemes",
+      "Disbanding Troops and Bureaucrats",
+      "The Fetish of Full Employment",
+      `Who's "Protected" by Tariffs?`,
+      "The Drive for Exports",
+      `"Parity" Prices`,
+      "Saving the X Industry",
+      "How the Price System Works",
+      `"Stabilizing" Commodities`,
+      "Government Price-Fixing",
+      "What Rent Control Does",
+      "Minimum Wage Laws",
+      "Do Union Really Raise Wages",
+      `"Enough to Buy Back the Product"`,
+      "The Function of Profits",
+      "The Mirage of Inflation",
+      "The Assault on Saving",
+      "The Lesson Restated",
+      "The Lesson After Thirty Years",
+    ],
+
+    quotes: [
+      {
+        txt:
+          "Economics is haunted by more fallacies than any other study known to man.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "The bad economist sees only the direct consequences of a proposed course; the good economist looks also at the longer and indirect consequences.",
+        chapter: 1,
+      },
+      // {
+      //   txt:
+      //     "Doesn't everybody know, in his personal life, that there are all sorts of indulgences delightful at the moment but disastrous in the end?",
+      //   chapter: 1,
+      // },
+      // {
+      //   txt:
+      //     "The art of economics consists in looking not merely at the immediate but at the the longer effects of any act or policy; it consists in tracing the consequences of that policy not merely for one group but for all groups.",
+      //   chapter: 1,
+      // },
+      {
+        txt:
+          "It is often sadly remarked that the bad economists present their errors to the public better than the good economists present their truths.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "the demagogues and bad economists are presenting half-truths. They are speaking only of the immediate effect of a proposed policy or its effects upon a single group. As far as they go they may often be right.",
+        chapter: 1,
+      },
+      // {
+      //   txt:
+      //     "the lesson will not be driven home, and the fallacies will continue to go unrecognized, unless both are illustrated by examples.",
+      //   chapter: 1,
+      // },
+      // {
+      //   txt:
+      //     "A young hoodlum, heaves a brick through the window of a baker's shop.",
+      //   chapter: 2,
+      // },
+      {
+        txt:
+          "(Yet) the broken-window fallacy, under a hundred disguises, is the most persistent in the history of economics",
+        chapter: 3,
+      },
+      {
+        txt:
+          "The more war destoys, the more it impoverishes, the greater is the postwar need. Indubitably. But need is not demand. Effective economic demand requires not merely need but corresponding purchasing power.",
+        chapter: 3,
+      },
+      // {
+      //   txt:
+      //     "But as most people are so firmly in the habit of thinking of their wealth and income in terms of money, they consider themselves better off as these monetary totals rise, in spite of the fact that in terms of things they may have less and buy less.",
+      //   chapter: 3,
+      // },
+      {
+        txt:
+          "No man burns down his own house on the theory that the need to rebuild it will stimulate his energies.",
+        chapter: 3,
+      },
+      // {
+      //   txt:
+      //     "Many of the most frequent fallacies in economic reasoning come from the propensity, especially marked today, to think in terms of an abstraction-the collectivity, the 'nation'-and to forget or ignore the individuals who make it up and give it meaning.",
+      //   chapter: 3,
+      // },
+      {
+        txt:
+          "The supply of the thing they make is all that people have, in fact, to offer in exchange for the things they want.",
+        chapter: 3,
+      },
+      {
+        txt:
+          "Everywhere government spending is presented as a panacea for all our economics ills.",
+        chapter: 4,
+      },
+      {
+        txt:
+          "The world is full of so-called economists who in turn are full of schemes for getting something for nothing.",
+        chapter: 4,
+      },
+      // {
+      //   txt:
+      //     "inflation itself is merely a form, and a particularly vicious form, of taxation.",
+      //   chapter: 4,
+      // },
+      {
+        txt:
+          "every dollar of governement spending must be raised through a dollar of taxation.",
+        chapter: 4,
+      },
+      // {
+      //   txt:
+      //     "for every public job created by the bridge project a private job has been destroyed somewhere else",
+      //   chapter: 4,
+      // },
+      {
+        txt:
+          "the cost to the taxpayers is spread over many years instead of being concentrated into one.",
+        chapter: 4,
+      },
+      {
+        txt:
+          "The jobs destroyed by the taxes for the housing are not seen, nor are the goods and services that were never made.",
+        chapter: 4,
+      },
+      {
+        txt:
+          "Again we must make an effort of the imagination to see the private power plants, the private homes, the typewriters and television sets that were never allowed to come into existence because of the money that was taken from people all over the country to build the photogenic Norris Dam.",
+        chapter: 4,
+      },
+      {
+        txt:
+          "the wealth created by government spending will fully compensate for the wealth destroyed by the taxes imposed to pay for that spending",
+        p: 37,
+        chapter: 5,
+      },
+      {
+        txt: "B is seen. A is forgotten.",
+        p: 37,
+        chapter: 5,
+      },
+      {
+        txt:
+          "If they lose the whole dollar when they lose, but can keep only a fraction of it when they win, they decide that it is foolish to take risks with their capital.",
+        p: 38,
+        chapter: 5,
+      },
+      {
+        txt:
+          "capital to provide new private jobs is first prevented from coming into existence, and the part that does come into existence is then discouraged from starting new enterprises.",
+        p: 38,
+        chapter: 5,
+      },
+      {
+        txt:
+          "[But] the larger the percentage of the national income taken by taxes the greater the deterrent to private production and employment.",
+        p: 39,
+        chapter: 5,
+      },
+      {
+        txt: `Government "Encouragement" to business is sometimes as much to be feared as government hostility. This suppposed encouragement often takes the form of a direct grant of governemnt credit or a guarantee of private loans.`,
+        p: 40,
+        chapter: 6,
+      },
+      {
+        txt:
+          "Proposals for an increased volume of credit, therefore, are merely another name for proposals for an increased burden of debt.",
+        p: 41,
+        chapter: 6,
+      },
+      {
+        txt:
+          "the government lenders will take risks which other people's money (the taxpayers) that private lenders will not take with their own money.",
+        p: 42,
+        chapter: 6,
+      },
+      // {
+      //   txt: "What is put in the hands of B cannot be put into the hands of A.",
+      //   p: 45,
+      //   chapter: 6,
+      // },
+      {
+        txt:
+          "The government never lends or gives anything to business that it does no take away from business",
+        p: 47,
+        chapter: 6,
+      },
+      {
+        txt:
+          "When the government makes loans or subsidies to business, what it does is to tax successful private business, what it does is to tax successful private business in order to support unsuccessful private business.",
+        p: 47,
+        chapter: 6,
+      },
+      {
+        txt:
+          "Among the most viable of all economic delusions is the belief that machines on net balance create unemployment.",
+        p: 49,
+        chapter: 7,
+      },
+      {
+        txt:
+          "If it were indeed true that the introduction of labor-saving machinery is a cause of constantly mounting unemployment and misery, the logical conclusions to be drawn would be revolutionary, not only in the technical fields but for out whole concept of civilization. Not only should we have to regard all further technical progress as a calamity; we should have to regard all past technical progress with equal horror.",
+        p: 54,
+        chapter: 7,
+      },
+      {
+        txt:
+          "Why should freight be carried from Chicago to New York by railroad when we could employ enormously more men, for example, to carry it all on their backs?",
+        p: 54,
+        chapter: 7,
+      },
+      {
+        txt:
+          "The real result of the machine is to increase *production*, to raise the standard of living, to increase economic welfare.",
+        p: 58,
+        chapter: 7,
+      },
+      {
+        txt: `But what they then proceed to do is to keep their eyes *only* on Joe Smith, and to forget Tom Jones, who has just got a job operating one, and Daisy Miller, who can now buy a coat for half what it used to cost her.`,
+        p: 59,
+        chapter: 7,
+      },
+      {
+        txt:
+          "The householder who is forced to employ two men to do the work of one has, it is true, given employment to one extra man. But he has just that much less money left over to spend on something that would employ somebody else.",
+        p: 62,
+        chapter: 8,
+      },
+      {
+        txt:
+          "There is no limit to the amount of work to be done as long as any human or wish that work could fill remains unsatisfied.",
+        p: 66,
+        chapter: 8,
+      },
+      {
+        txt:
+          "When your money is taken by a thief, you get nothing in return. When your money is taken through taxes to support needless bureaucrats, precisely the same situation exists.",
+        p: 70,
+        chapter: 9,
+      },
+      {
+        txt:
+          "We cannot continuously have the fullest production without full employment. But we can very easily have full employment without full production.",
+        p: 71,
+        chapter: 10,
+      },
+      {
+        txt:
+          "Nothing is easier to achieve than full employment, once it is divorced from the goal of full production and taken as an end in itself.",
+        p: 72,
+        chapter: 10,
+      },
+      {
+        txt: "Prisons and chain gangs have full employment.",
+        p: 72,
+        chapter: 10,
+      },
+      {
+        txt:
+          "Yet our legislators do not present Full Production bills in Congress but Full Employment bills.",
+        p: 72,
+        chapter: 10,
+      },
+      {
+        txt:
+          "The progress of civilization has meant the reduction of employment, not its increase.",
+        p: 72,
+        chapter: 10,
+      },
+      {
+        txt:
+          "The real question is not how many millions of jobs there will be in America ten years from now, but how much shall we produce, and what, in consequence, will be our standard of living?",
+        p: 73,
+        chapter: 10,
+      },
+      {
+        txt:
+          "Labor in each country is more fully employed in doing those things that it does best, instead of being forced to do things that it does inefficiently or badly.",
+        p: 77,
+        chapter: 11,
+      },
+      {
+        txt:
+          "[But] as a result of the articifial barrier erected against foreign goods, American labor, capital and land are deflected from what they can do more efficently to what they do less efficiently. Therefore, as a result of the tariff wall, the average productivity of American labor and capital is reduced.",
+        p: 79,
+        chapter: 11,
+      },
+      {
+        txt:
+          "On the one hand, we try to reduce the cost of transportation between England and America, or Canada and the United States, by developing faster and more efficient planes and ships, better roads and bridges, better locomotives or motor trucks. On the other hand, we offset this investment in efficient transportation by a tariff that makes it commercially even more difficult to transport goods than it was before.",
+        p: 80,
+        chapter: 11,
+      },
+      {
+        txt:
+          "The effect of a tariff, therefore, is to change the *structure* of American production. It changes the number of occupations, the kind of occupations, and the relative size of one industry as compared with another. It makes the industries in which we are comparatively inefficient larger, and the industries in which we are comparatively efficient smaller.",
+        p: 82,
+        chapter: 11,
+      },
+      {
+        txt:
+          "It is useless to deny that a tariff does benefit⁠—or at least *can* benefit⁠—*special interests*. True, it benefits them *at the expense of everyone else*.",
+        p: 83,
+        chapter: 11,
+      },
+      {
+        txt:
+          "even people in the protected industries, both as producers and consumers, begin to suffer from other people's production, and may finally be worse off even on net balance than if neither they nor anybody else had protection.",
+        p: 83,
+        chapter: 11,
+      },
+      {
+        txt:
+          "Without exports we can have no exports, for foreigners will have no funds with which to buy our goods.",
+        p: 85,
+        chapter: 12,
+      },
+      {
+        txt:
+          "Each of us must also sell something, even if for most of us it is our own services rather than goods, in order to get the purchasing power to buy.",
+        p: 86,
+        chapter: 12,
+      },
+      {
+        txt: `If an automobile company lends a man $5,000 to buy a car priced at that amount, and the load is not repaid, the automobile company is not better off because it has "sold" the car.`,
+        p: 87,
+        chapter: 12,
+      },
+      {
+        txt:
+          "Collectively considered, the real reason a country needs export is to pay for its imports.",
+        p: 89,
+        chapter: 12,
+      },
+      {
+        txt: "B is seen. A is forgotten",
+        p: 37,
+        chapter: 13,
+      },
+      {
+        txt: "B is seen. A is forgotten",
+        p: 37,
+        chapter: 13,
+      },
+      {
+        txt: "B is seen. A is forgotten",
+        p: 37,
+        chapter: 13,
+      },
+      {
+        txt: "B is seen. A is forgotten",
+        p: 37,
+        chapter: 13,
+      },
+    ],
+  },
+  ["long-walk-to-freedom"]: {
+    title: "Long Walk To Freedom",
+    author: "Nelson Mandela",
+    date: 1995,
+    quotes: [
+      {
+        txt:
+          "There is nothing like returning to a place that remains unchanged to find the ways in which you yourself have altered.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "Politics, in my experience, is nothing but a racket to steal money from the poor.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "Laws stripping people of their rights were inevitably described as laws restoring those rights.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "It was a crime to walk through a Whites Only door, a crime to ride a Whites Only bus, a crime to use a Whites Only drinking fountain, a crime to walk on a Whites Only beach, a crime to be on the streets past eleven, a crime not to have a pass book and a crime to have the wrong signature in that book, a crime to be unemployed and a crime to be employed in the wrong place, a crime to live in certain places and a crime to have no place to live.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "It is what we make out of what we have, not what we are given, that separates one person from another.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "newspapers are only a poor shadow of reality; their information is important to a freedom fighter not because it reveals the truth, but because it discloses the biases and perceptions of both those who produce the paper and those who read it.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "It is said that no one truly knows a nation until one has been inside its jails. A nation should not be judged by how it treats its highest citizens, but its lowest ones-and South Africa treated its imprisoned African citizens like animals.",
+        chapter: 1,
+      },
+      {
+        txt:
+          "When the women begin to take an active part in the struggle, no power on earth can stop us from achieving freedom in our lifetime.",
         chapter: 1,
       },
     ],
