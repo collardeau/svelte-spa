380769059246078d6a321af67dc8ed85de3f4f54
Thomas Collardeau <thomas@collardeau.com>
2021-03-29 17:36:35 +0200

    add ontheinside

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
 export const works = [
   {
     href: "#/target",
-    title: "Target"
+    title: "Target",
   },
   {
     href: "https://john-hong-studio.com",
-    title: "John Hong Studio"
+    title: "John Hong Studio",
   },
   {
     href: "http://fadeinfilms.com",
-    title: "Fade In Films"
-  }
+    title: "Fade In Films",
+  },
   // artjunk, cleverhire, oti
 ];
 
@@ -19,34 +19,39 @@ export const targetLinks = [
     href:
       "https://www.fastcompany.com/90177570/target-has-a-secret-app-for-superfans-and-it-looks-like-instagram",
     text: "Target has a secret app for superfans, and it looks like Instagram",
-    source: "fastcompany.com"
+    source: "fastcompany.com",
   },
   {
     href:
       "https://www.businessinsider.com/target-studio-connect-lets-shoppers-designers-collaborate2018-7?r=US&IR=T",
     text: "A new Target app connects customers to the design team",
-    source: "businessinsider.com"
+    source: "businessinsider.com",
   },
   {
     href:
       "https://www.bustle.com/p/targets-app-studio-connect-invites-super-fans-of-the-store-to-collaborate-with-designers-9788159",
     text: `Target’s App “Studio Connect” Invites Super Fans Of The Store To
     Collaborate With Designers`,
-    source: "bustle.com"
-  }
+    source: "bustle.com",
+  },
 ];
 export const projects = [
   {
     href: "https://kairos-weather.netlify.com/",
     // href: "https://kairos.now.sh/",
     title: "Kairos",
-    descr: "find sunny destinations"
+    descr: "find sunny destinations",
+  },
+  {
+    href: "https://ontheinside.info/",
+    title: "On The Inside",
+    descr: "personality driven city guide",
   },
   {
     // href: "https://travel-info.online",
     href: "https://travel-info.netlify.com/",
     title: "Travel Info",
-    descr: "easy travel searches"
+    descr: "easy travel searches",
   },
   // {
   //   href: "http://conceptor-app.com",
@@ -58,8 +63,8 @@ export const projects = [
     // href: "https://life-on-bicycles.com",
     href: "https://life-on-bicycles.netlify.com",
     title: "Life On Bicycles",
-    descr: "promotion bicycle culture"
-  }
+    descr: "promotion bicycle culture",
+  },
   // {
   //   href: "http://karaoke.collardeau.com/",
   //   txt: "Karaoke Machine",
@@ -72,25 +77,25 @@ export const blogs = [
   {
     href: "https://dev.to/collardeau/props-up-with-svelte-4cjk",
     title: "Props up with Svelte",
-    source: "dev.to"
+    source: "dev.to",
   },
   {
     href: "https://dev.to/collardeau/lazy-loading-images-in-svelte-1mk6",
     title: "Lazy Loading Images in Svelte",
-    source: "dev.to"
+    source: "dev.to",
   },
   {
     href:
       "https://medium.com/@collardeau/a-committed-guide-to-reactjs-47c0371df3ab",
     title: "A Committed Intro Guide to ReactJS",
-    source: "medium.com"
+    source: "medium.com",
   },
   {
     href:
       "https://medium.com/@collardeau/intro-to-functional-programming-concepts-in-javascript-b0650773139c",
     title: "An Intro to Functional Programming Concepts",
-    source: "medium.com"
-  }
+    source: "medium.com",
+  },
   // react-lil-router
 ];
 
@@ -98,28 +103,28 @@ export const social = {
   github: {
     href: "https://github.com/collardeau",
     handle: "collardeau",
-    name: "github"
+    name: "github",
   },
   twitter: {
     href: "https://twitter.com/collardeau",
     handle: "@collardeau",
-    name: "twitter"
+    name: "twitter",
   },
   devTo: {
     href: "https://dev.to/collardeau",
     handle: "collardeau",
-    name: "dev.to"
+    name: "dev.to",
   },
   vimeo: {
     href: "https://vimeo.com/collardeau",
     handle: "collardeau",
-    name: "vimeo"
+    name: "vimeo",
   },
   linkedIn: {
     href: "https://linkedin.com/in/collardeau",
     handle: "collardeau",
-    name: "linkedin"
-  }
+    name: "linkedin",
+  },
   // stackoverflow, runkeeper, goodread
 };
 
@@ -130,5 +135,5 @@ export const me = {
   starting with usage of JQuery in my early days, then AngularJS (much to my chagrin) and embracing React in 2015(?). Currently, my favorite stack
   incude Sapper/Svelte and Firebase.</p><p>I'm originally from France and have lived in the US, The Netherlands, Spain and the United Kingdom.</p>In my free time, I love to travel to far away lands, paddle board on the open sea, and learn to play guitar.</p><p>In 2006, I was named TIME magazine's Person of the Year.</p>`,
   email: "tomadeau [at] gmail [dot] com",
-  social
+  social,
 };
